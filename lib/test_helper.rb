36b363604fdf2bc7898415341947b684f34feb8a
everysum1 <israeldmatos@gmail.com>
2017-11-02 08:46:35 -0700

    Add test_helper file

---
+++ lib/test_helper.rb
@@ -0,0 +1,9 @@
+require 'bundler/setup'
+require 'yaml'
+require 'marqeta'
+require 'pry'
+require 'faker'
+
+## Test data
+require_relative '../lib/test_data/data_helper'
+require_relative '../lib/rest_client_wrapper'
