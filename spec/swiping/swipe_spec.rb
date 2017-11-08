1a4d75a0bd4d7c1942e52a1602061240727f6593
everysum1 <israeldmatos@gmail.com>
2017-11-07 20:22:42 -0800

    Add new modular test cases to swipe spec

--- spec/swiping/swipe_spec.rb
+++ spec/swiping/swipe_spec.rb
@@ -14,11 +14,22 @@ describe 'Swipe like there\'s no tomorrow', :test do
 
     it 'should open up the main swipe page' do
       driver.get @base_url
-      expect(driver.title).to eq "Tinder"
+      expect(driver.title).to eq "Tinder | Match. Chat. Meet. Modern Dating."
+    end
+
+    it 'should open the sign in modal' do
+      driver.find_element(SwipePage::FACEBOOK_BUTTON).click
+      sleep 60
+    end
+
+    it 'should sign in with facebook credentials' do
+      driver.find_element(SwipePage::EMAIL_INPUT).send_keys
+      driver.find_element(SwipePage::PASSWORD_INPUT).send_keys
     end
 
     it 'should swipe right until it hits 10000 swipes' do
       like_button = driver.find_element(SwipePage::LIKE_BUTTON)
+      sleep 30
 
       10000.times do
         like_button.click
